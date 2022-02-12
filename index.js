a8fb59f1e75e9a6bf3c37783bcb2105004ca707d
tnemnorivnelee <tnemnorivnelee@gmail.com>
2022-02-12 22:37:49 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 5000
 
-const mongoose = require('mongoose')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+// application/x-www.form-url-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+// application/json
+app.use(express.json())
 
 mongoose
-	.connect(
-		'mongodb+srv://asdf:asdf1234@boilerplate.6hakg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-	)
+	.connect(config.mongoURI)
 	.then(() => {
 		console.log('MongoDB Connected...')
 	})
@@ -15,7 +21,24 @@ mongoose
 		console.log(err)
 	})
 
-app.get('/', (req, res) => res.send('Hello world!'))
+app.get('/', (req, res) => {
+	res.send('Hello world!!!!!!!!!')
+})
+
+app.post('/register', (req, res) => {
+	const user = new User(req.body)
+	user.save((err, userInfo) => {
+		if (err) {
+			return res.json({
+				success: false,
+				err,
+			})
+		}
+		return res.status(200).json({
+			success: true,
+		})
+	}) // mongoDB Method
+})
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}!!`)
